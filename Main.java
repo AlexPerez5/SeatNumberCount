47136e7b378f93200f051271a966be662dcb3038
Alexander Perez <alexander.perez@patelhs.org>
2020-11-14 04:34:16 +0000

    Code for student names

--- Main.java
+++ Main.java
@@ -6,7 +6,10 @@ class Main { //beginning//
   Scanner scan = new Scanner(System.in);
 
   //Number of seats//
-  
+  int[] Numseats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  String[] Namesstud = new String[15]; //Code for student names//
+
+
 
   }
 }
