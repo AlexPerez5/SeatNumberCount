a0380cb9a0ab6a0c263be71b546ee9f3bb570b7d
Alexander Perez <alexander.perez@patelhs.org>
2020-11-14 04:59:35 +0000

    Code that allows 15 lines of names

--- Main.java
+++ Main.java
@@ -6,8 +6,20 @@ class Main { //beginning//
   Scanner scan = new Scanner(System.in);
 
   //Number of seats//
-  int[] Numseats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-  String[] Namesstud = new String[15]; //Code for student names//
+  int[] seatNums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+  String[] studNames = new String[15]; //Code that prints the names 15 times//
+  
+  //Code for student names//
+
+  System.out.println("Enter 15 Random Students Names..");
+  for(int a = 0; a < 15; a++)//Code for 15 lines of names//
+  {
+    String namesStud = scan.next();
+    studNames[a] = namesStud;
+  }
+
+
+
 
 
 
